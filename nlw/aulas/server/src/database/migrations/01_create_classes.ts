fda4f362412de5a0c5767be9bea60c259d0d2049
Daniel <38465439+dalvesc@users.noreply.github.com>
2020-08-06 00:11:33 -0300

    server #Aula02

---
+++ nlw/aulas/server/src/database/migrations/01_create_classes.ts
@@ -0,0 +1,23 @@
+import Knex from "knex";
+
+//quais alterações quer no banco
+export async function up(knex: Knex) {
+  return knex.schema.createTable("classes", (table) => {
+    table.increments("id").primary();
+    table.string("subject").notNullable();
+    table.decimal("cost").notNullable();
+
+    table
+      .integer("user_id")
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
+}
+
+//deu merda o que fazer pra voltar
+export async function down(knex: Knex) {
+  return knex.schema.dropTable("classes");
+}
